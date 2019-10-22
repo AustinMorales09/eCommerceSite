1488a3f0940f3b52505a4a19a0b07307877da96f
AustinMorales <54542173+AustinMorales09@users.noreply.github.com>
2019-10-22 14:46:38 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "vhss2odfspz1",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "Y6RSYnBYwRFCNbW3TCMKiFGXyWihLlLNQV7BITm0-8A"
+  });
+//variables 
+console.log(client);
+
+
 const cartBtn =document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
@@ -17,9 +27,15 @@ let buttonsDOM = [];
 class Products{
     async getProducts(){
         try{
-            let result = await fetch('js/products.json')
-            let data = await result.json();
-            let products =data.items;
+           let contentful = await client.getEntries({
+               content_type: "comfyhouseproduct"
+           });
+
+           
+            // let result = await fetch('js/products.json')
+            // let data = await result.json();
+
+            let products = contentful.items;
             products = products.map(item =>{
                 const {title,price} = item.fields;
                 const {id} = item.sys
@@ -43,7 +59,7 @@ class UI {
                     <img src=${product.image} alt="product" class="product-img">
                     <button class="bag-btn" data-id=${product.id}>
                         <i class="fas fa-shopping-cart"></i>
-                        add to bag
+                        add to cart
                     </button>
                 </div>
                 <h3>${product.title}</h3>
@@ -66,9 +82,8 @@ class UI {
             }
             
                 button.addEventListener('click',(event)=>{
-                    console.log(event);
                     event.target.innerText = "In Cart";
-                    event.target.disabled = true;
+                    event.target.disabled = false;
 
                     // get product from products
                     let cartItem = {...Storage.getProduct(id), amount:1};
@@ -146,12 +161,47 @@ class UI {
         clearCartBtn.addEventListener('click', () => {
             this.clearCart();
         });
+
+        cartContent.addEventListener('click', event=> {
+            if(event.target.classList.contains('remove-item'))
+        {
+            let removeItem = event.target;
+            let id = removeItem.dataset.id;
+            cartContent .removeChild
+            (removeItem.parentElement.parentElement)
+            this.removeItem(id);
+        }
+        else if(event.target.classList.contains("fa-chevron-up")){
+            let addAmount = event.target;
+            let id = addAmount.dataset.id;
+            let tempItem = cart.find(item => item.id===id);
+            tempItem.amount = tempItem.amount + 1;  
+            Storage.saveCart(cart);
+            this.setCartValues(cart);
+            addAmount.nextElementSibling.innerText = tempItem.amount;
+        }
+        else if(event.target.classList.contains("fa-chevron-down")){
+            let lowerAmount = event.target;
+            let id = lowerAmount.dataset.id;
+            let tempItem = cart.find(item => item.id===id);
+            tempItem.amount = tempItem.amount - 1;
+            if(tempItem.amount > 0){
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                lowerAmount.previousElementSibling.innerText = tempItem.amount;
+             } else {
+                    cartContent.removeChild(lowerAmount.parentElement.parentElement);
+                    this.removeItem(id);
+                }
+            }
+        });
     }
     clearCart() {
        let cartItems = cart.map(item => item.id);
        cartItems.forEach(id => this.removeItem(id));
        while(cartContent.children.length>0){
            cartContent.removeChild(cartContent.children[0])
+           console.log(cartContent.children);
        }
     }
     removeItem(id) {
@@ -159,8 +209,8 @@ class UI {
         this.setCartValues(cart);
         Storage.saveCart(cart);
         let button = this.getSingleButton(id);
-        button.disable = false;
-        button.innerHTML = `<i class="fas fa-shopping-cart></i>add to cart`;
+        button.disabled = false;
+        button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
     }
     getSingleButton(id) {
         return buttonsDOM.find(button => button.dataset.id === id);
